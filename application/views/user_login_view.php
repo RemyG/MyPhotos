c0f6f4222b3ecd265f4eec9dd21e5583515f94c2
RemyG <remy.gardette@gmail.com>
2014-09-10 09:21:29 +0100

    Fixed indentation and removed empty action attribute

--- application/views/user_login_view.php
+++ application/views/user_login_view.php
@@ -3,20 +3,20 @@
 	<div class="section_title">
 		<h2>Connection</h2>
 	</div>
-	
+
 	<div class="section_content">
 
-		<form method="post" action="">
-		
+		<form method="post">
+
 			<label for='hash'>Login</label>
 			<input type='text' name='hash' id='hash' value='<?php echo $userhash; ?>' autofocus="autofocus" /><br/>
 			<label for='password'>Password</label>
 			<input type='password' name='password' id='password' /><br/>
-		
+
 			<input type="submit" class="btn" name="login" value="Login" />
-		
+
 		</form>
 
 	</div>
-	
+
 </div>
